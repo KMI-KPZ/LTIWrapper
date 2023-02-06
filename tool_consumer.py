79b96cbc34e691deb767d58c46b6d23a3c2479ed
Smunfr <christian.schlecht97@web.de>
2023-02-06 10:22:19 +0100

    add consumer example that displays tool response in an iframe

--- tool_consumer.py
+++ tool_consumer.py
@@ -5,6 +5,9 @@ from requests_oauthlib.oauth1_auth import SIGNATURE_TYPE_BODY
 from requests_oauthlib import OAuth1
 import urllib.parse
 
+import tornado
+import tornado.web
+
 
 class LTIToolConsumer:
     """
@@ -53,7 +56,9 @@ class LTIToolConsumer:
         self.lti_message_type = "basic-lti-launch-request"
         self.lti_version = "LTI-1p0"
 
-    def launch_request(self, extra_params: Dict = None) -> Tuple[int, dict, str]:
+    def launch_request(
+        self, extra_lti_params: Dict = None, **kwargs
+    ) -> Tuple[int, dict, str]:
         """
         Consume the LTI Tool by sending a request to the provider.
         The request is signed with a OAuth1.0 HMAC_SHA1 signature as per the LTI standard.
@@ -62,7 +67,8 @@ class LTIToolConsumer:
         By doing so, default parameter values such as e.g. `lti_message_type`, or `lti_version` may
         be overwritten.
 
-        :param extra_params: dict containing extra key/value pairs to append to the request
+        :param extra_lti_params: dict containing extra key/value pairs to append to the request
+        :param **kwargs: extra arguments to pass to requests, see `requests.post()` for details.
 
         :returns: a tuple containing the relevant response information: HTTP status code,
         HTTP Response Headers, Reponse Content (LTI tool itself)
@@ -73,14 +79,14 @@ class LTIToolConsumer:
         # to prevent blank values from getting deleted and as such messing up the signature.
         # there is probably a smarter way, however this works for now
         request = self.sign_request(
-            self.generate_launch_request_payload(extra_params=extra_params)
+            self.generate_launch_request_payload(extra_params=extra_lti_params)
         )
         payload_body = self.parse_qs(
             request.body.decode("utf8"), keep_blank_values=True
         )
 
         # get LTI resource from the Tool Provider
-        response = requests.post(self.launch_url, payload_body)
+        response = requests.post(self.launch_url, payload_body, **kwargs)
 
         # return status code, headers and parsed html content
         return (
@@ -143,15 +149,79 @@ class LTIToolConsumer:
         return sign(request)
 
 
-if __name__ == "__main__":
-    consumer_key = "<key_from_provider>"
-    consumer_secret = "<secret_from_provider>"
-    launch_url = "<launch_url_from_provider>"
+class ToolConsumerExample(tornado.web.RequestHandler):
+    """
+    Small example Tornado Handler that requests the Tool from the provider
+    and displays it on localhost:8080 in an iframe.
 
+    This example has been testet with Moodle, which replies to the launch request
+    with a "303 See Other" response. Therefore we don't follow this redirect but
+    set the iframe's src URL from the "Location"-Header.
+
+    In case you try it with a different provider, you might have to change the display
+    logic accordingly.
+    """
+
+    def get(self):
+        global consumer_key
+        global consumer_secret
+        global launch_url
+
+        # do the launch request
+        response = LTIToolConsumer(
+            consumer_key, consumer_secret, launch_url
+        ).launch_request(allow_redirects=False)
+
+        # display content in an iframe
+        # (adapted especiall for Moodle's "303 See Other" response)
+        self.write(
+            """
+            <h1>tool from Provider:</h1>
+            <br/>
+            <iframe width=100% height=100% src={}></iframe>
+            """.format(
+                response[1]["Location"]
+            )
+        )
+
+
+def make_app():
+    return tornado.web.Application([(r"/", ToolConsumerExample)])
+
+
+if __name__ == "__main__":
+    #######################################################
+    # CHANGE THOSE VALUES ACCORDING TO YOUR TOOL PROVIDER #
+    #######################################################
+    consumer_key = "key_from_provider"
+    consumer_secret = "seret_from_provider"
+    launch_url = "launch_url_from_provider"
+    #######################################################
+    #                                                     #
+    #######################################################
+
+    print(
+        "To view an example visualization of the tool you just consumed, go to {}".format(
+            "http://localhost:8080/"
+        )
+    )
+    print(
+        "Additionally, the response from the provider has been logged to {}".format(
+            "consumer_log.txt"
+        )
+    )
+
+    # do the launch request and log it to file for debugging
     response = LTIToolConsumer(
         consumer_key, consumer_secret, launch_url
-    ).launch_request()
+    ).launch_request(allow_redirects=False)
 
-    print(response[0])
-    print(response[1])
-    print(response[2])
+    with open("consumer_log.txt", "a") as log:
+        log.write(str(response[0]) + "\n")
+        log.write(str(response[1]) + "\n")
+        log.write(str(response[2]) + "\n")
+
+    # start example
+    app = make_app()
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
